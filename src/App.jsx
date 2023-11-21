f4b961110f86c9d640e5144060d40cb8be8444b4
Jonathan Fenoud-Viard <jonathan.fenoud-viard@outlook.fr>
2023-11-21 11:05:35 +0100

    ✨ F02: Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import MainRoute from './Routes'
+import LoginPage from './components/pages/login/LoginPage'
+import OrderPage from './components/pages/order/OrderPage'
+import ErrorPage from './components/pages/error/ErrorPage'
+import { Routes, Route } from 'react-router-dom'
 
 
 
 
 function App() {
+
   return (
-    <MainRoute />
+    <Routes>
+      <Route path="/" element={<LoginPage/>}/>
+      <Route path="/order" element={<OrderPage/>}/>
+      <Route path="*" element={<ErrorPage/>}/>
+    </Routes>
   )
 }
 
